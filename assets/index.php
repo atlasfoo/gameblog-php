b05f9c96d13c2e7280d7c2a9e7befad1a6667072
atlasfoo@lin <jf_mejiar547@live.com>
2021-01-02 23:47:18 -0600

    maquetacion principal

--- assets/index.php
+++ assets/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <!--Header-->
-    <header>
+    <header id="cabecera">
         <div id="logo">
             <a href="index.php">
                 <h1>Blog Gaimer</h1>
@@ -15,7 +15,7 @@
         </div>
 
         <!--Menu-->
-        <nav id="nav">
+        <nav id="menu">
             <ul>
                 <li>
                     <a href="index.html">Inicio</a>
@@ -32,8 +32,52 @@
             </ul>
         </nav>
     </header>
-    <!--Sidebar-->
-    <!--Main-->
+    <div id="contenedor">
+        <!--Sidebar-->
+        <aside id="sidebar">
+            <section id="login" class="bloque">
+                <h3>Inicia Sesion</h3>
+                <form action="login.php" method="post">
+                    <label for="email">Email</label>
+                    <input type="email" name="email"/>
+
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password"/>
+
+                    <input type="submit" value="Entrar" />
+                </form>
+            </section>
+
+            <section id="register" class="bloque">
+                <h3>Registrate</h3>
+                <form action="register.php" method="post">
+                    <label for="name">Nombres</label>
+                    <input type="text" name="name"/>
+
+                    <label for="lastname">Apellidos</label>
+                    <input type="text" name="lastname"/>
+
+
+                    <label for="email">Email</label>
+                    <input type="email" name="email"/>
+
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password"/>
+
+                    <input type="submit" value="Registrarse" />
+                </form>
+            </section>
+
+        </aside>
+        <!--Main-->
+        <main id="principal">
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <h2>Titulo de entrada</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque odio quaerat aliquid fugiat alias sapiente blanditiis eaque eos impedit magni eum esse placeat culpa sit, vero at dolorem a atque.</p>
+            </article>
+        </main> 
+    </div>
     <!--Footer-->
 </body>
 </html>

a8dd9ec70a198d05c207db24fd47839eb468444a
atlasfoo@lin <jf_mejiar547@live.com>
2021-01-05 22:20:52 -0600

    user registration form function part 2

--- register.php
+++ register.php
@@ -4,13 +4,40 @@
 
         // recoger los campos
         $name = (isset($_POST['name']))? $_POST['name'] : false;
-        $last_name = (isset($_POST['lastname']))? $_POST['lastname'] : false;
+        $lastname = (isset($_POST['lastname']))? $_POST['lastname'] : false;
         $email = (isset($_POST['email']))? $_POST['email'] : false;
         $password = (isset($_POST['password']))? $_POST['password'] : false;
     
+        $errors = array();
+
         // validar los datos
-        if(!empty($name) && !is_numeric($name) && !preg_match("/[0-9]", $name))
-            echo 'El nombre es valido';
+        if(!empty($name) && !is_numeric($name) && !preg_match("/[0-9]", $name)) {
+            $name_valid = true;
+        } else {
+            $name_valid = false;
+            $errors['name'] = 'El nombre está vacío o no es válido';
+        }
+
+        if(!empty($lastname) && !is_numeric($lastname) && !preg_match("/[0-9]", $lastname)) {
+            $lastname_valid = true;
+        } else {
+            $lastname_valid = false;
+            $errors['lastname'] = 'El apellido está vacío o no es válido';
+        }
+
+        if(!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $email_valid = true;
+        } else {
+            $email_valid = false;
+            $errors['email'] = 'El correo está vacío o no es válido';
+        }
+
+        if(!empty($password)) {
+            $password_valid = true;
+        } else {
+            $password_valid = false;
+            $errors['password'] = 'La contraseña está vacía o no es válida';
+        }
     }
 
 ?>
